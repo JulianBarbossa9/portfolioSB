531ef47c85190d79bf706f599d6d57c45be92748
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-12 20:53:41 -0500

    update imgThree

--- class01/src/components/ImgThree.jsx
+++ class01/src/components/ImgThree.jsx
@@ -8,22 +8,22 @@ const ImgThree = () => {
     <>
         <mesh castShadow receiveShadow>
           <boxGeometry args={[2,2,2]} />
-          <Side rotation={[0,0,0]} bg='orange' index={0}> 
+          <Side rotation={[0,0,0]} bg='#3F6163' index={0}> 
             <torusGeometry args={[0.65, 0.3, 64]} />
           </Side>
-          <Side rotation={[0,Math.PI,0]} bg='lightblue' index={1}> 
+          <Side rotation={[0,Math.PI,0]} bg='#DDD9D0' index={1}> 
             <torusKnotGeometry args={[0.55, 0.2, 128, 32]} />
           </Side>
-          <Side rotation={[0,Math.PI / 2 ,Math.PI / 2]} bg='lightgreen' index={2}> 
+          <Side rotation={[0,Math.PI / 2 ,Math.PI / 2]} bg='#C2DFED' index={2}> 
             <boxGeometry args={[1.15, 1.15, 1.15]} />
           </Side>
-          <Side rotation={[0,Math.PI / 2 ,Math.PI / 2]} bg='aquamarine' index={3}> 
+          <Side rotation={[0,Math.PI / 2 ,-Math.PI / 2]} bg='#768B71' index={3}> 
             <octahedronGeometry />
           </Side>
-          <Side rotation={[0,Math.PI / 2 ,0]} bg='indianred' index={4}> 
+          <Side rotation={[0,-Math.PI / 2 ,0]} bg='#c14949' index={4}> 
             <icosahedronGeometry />
           </Side>
-          <Side rotation={[0,Math.PI / 2 ,0]} bg='hotpink' index={5}> 
+          <Side rotation={[0,Math.PI / 2 ,0]} bg='#B6806A' index={5}> 
             <dodecahedronGeometry />
           </Side>
         </mesh>
---
+++ class01/src/components/ProjectItem.jsx
@@ -0,0 +1,74 @@
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const ProjectItemStyle = styled.div`
+    .projectItem__img{
+        width: 100%;
+        height: 400px;
+        overflow: hidden;
+        border-radius: 12px;
+        display: inline-block;
+        border: 3px solid var(--gray-2);
+        img{
+            height: 100%;
+        }
+    }
+    .projectItem__info{
+        margin-top: 0.5rem;
+        background-color: var(--deep-dark);
+        padding: 0.5rem;
+        border-radius:12px;
+        /* border: 2px solid red; */
+        /* width:600px; */
+    }
+    .projectItem__title{
+        font-size: 1rem;
+    }
+    .projectItem__descrip{
+        font-size: 0.5rem;
+        font-family: 'RobotoMono Regular';
+        margin-top: 0.5rem;
+    }
+    .projectButton{
+        display:inline-block;
+        font-size: 0.5rem;
+        text-decoration: underline;
+        color: royalblue;
+    }
+    @media only screen and (max-width: 768px){
+        .projectItem__img{
+            height: 305px;
+        }
+        .projectItem__descrip{
+            font-size: 0.5rem;
+        }
+    }
+`;
+
+const ProjectItem = ({img, title, descrip, link}) => {
+    return (  
+        <Fragment>
+            <ProjectItemStyle>
+            <Link to="" className="projectItem__img">
+                <img src={img} alt="project img"/>
+            </Link>
+            <div className="projectItem__info">
+                <Link to="/#">
+                    <h3 className="projectItem__title">
+                        {title}
+                    </h3>
+                </Link>
+                <p className="projectItem__descrip">
+                    {descrip}
+                </p>
+
+                {link && <a className='projectButton' target="_blank" rel="noreferrer"  href={link}>Open Project</a>}
+            </div>
+            </ProjectItemStyle>
+        </Fragment>
+    );
+}
+ 
+export default ProjectItem;
